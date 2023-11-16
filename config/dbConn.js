a67773bbce1a605eca6a9925cf6fc77b1b57773d
moshon12345 <107131721+moshon12345@users.noreply.github.com>
2023-11-16 22:07:56 +0200

    Update dbConn.js

--- config/dbConn.js
+++ config/dbConn.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try {
-        // await mongoose.connect(process.env.DATABASE_URI)
-        await mongoose.connect('mongodb://127.0.0.1/business_card_app')
+        await mongoose.connect(process.env.DATABASE_URI)
+        // await mongoose.connect('mongodb://127.0.0.1/business_card_app')
     } catch (err) {
         console.log(err)
     }
